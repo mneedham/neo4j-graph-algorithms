3eaf0e1e6caa4899e6fcf8dcd007712bdbba5ac7
Martin Knobloch <mknblch@gmail.com>
2018-10-09 15:43:00 +0200

    WIP

---
+++ algo/src/main/java/org/neo4j/graphalgo/impl/MapEquation.java
@@ -0,0 +1,21 @@
+package org.neo4j.graphalgo.impl;
+
+/**
+ * @author mknblch
+ */
+public class MapEquation extends Algorithm<MapEquation> {
+
+
+
+
+    @Override
+    public MapEquation me() {
+        return this;
+    }
+
+    @Override
+    public MapEquation release() {
+        // TODO
+        return this;
+    }
+}
