14cde42602fd3a96321cc43eb40cb89799bc4603
Mark Needham <m.h.needham@gmail.com>
2018-12-11 12:18:35 +0000

    remove unused

--- algo/src/main/java/org/neo4j/graphalgo/similarity/DenseWeightedInput.java
+++ algo/src/main/java/org/neo4j/graphalgo/similarity/DenseWeightedInput.java
@@ -19,9 +19,7 @@ class DenseWeightedInput implements WeightedInput {
     public DenseWeightedInput(long id, double[] weights) {
         this.id = id;
         this.weights = weights;
-        for (double weight : weights) {
-            this.count++;
-        }
+        count = weights.length;
     }
 
     public int compareTo(WeightedInput o) {
@@ -32,11 +30,7 @@ class DenseWeightedInput implements WeightedInput {
         int len = Math.min(weights.length, other.weights().length);
         double sumSquareDelta = Intersections.sumSquareDeltaSkip(weights, other.weights(), len, skipValue);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights[i] == other.weights[i] && weights[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && sumSquareDelta > similarityCutoff) return null;
         return new SimilarityResult(id, other.id(), count, other.count(), intersection, sumSquareDelta);
     }
@@ -45,11 +39,7 @@ class DenseWeightedInput implements WeightedInput {
         int len = Math.min(weights.length, other.weights().length);
         double sumSquareDelta = Intersections.sumSquareDelta(weights, other.weights(), len);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights[i] == other.weights[i] && weights[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && sumSquareDelta > similarityCutoff) return null;
         return new SimilarityResult(id, other.id(), count, other.count(), intersection, sumSquareDelta);
     }
@@ -58,11 +48,7 @@ class DenseWeightedInput implements WeightedInput {
         int len = Math.min(weights.length, other.weights().length);
         double cosineSquares = Intersections.cosineSquareSkip(weights, other.weights(), len, skipValue);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights[i] == other.weights[i] && weights[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && (cosineSquares == 0 || cosineSquares < similarityCutoff)) return null;
         return new SimilarityResult(id, other.id(), count, other.count(), intersection, cosineSquares);
     }
@@ -71,11 +57,7 @@ class DenseWeightedInput implements WeightedInput {
         int len = Math.min(weights.length, other.weights().length);
         double cosineSquares = Intersections.cosineSquare(weights, other.weights(), len);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights[i] == other.weights[i] && weights[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && (cosineSquares == 0 || cosineSquares < similarityCutoff)) return null;
         return new SimilarityResult(id, other.id(), count, other.count(), intersection, cosineSquares);
     }
--- algo/src/main/java/org/neo4j/graphalgo/similarity/SparseWeightedInput.java
+++ algo/src/main/java/org/neo4j/graphalgo/similarity/SparseWeightedInput.java
@@ -23,11 +23,7 @@ class SparseWeightedInput implements WeightedInput {
         int len = Math.min(weights().length, other.weights().length);
         double sumSquareDelta = Intersections.sumSquareDeltaSkip(weights(), other.weights(), len, skipValue);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights()[i] == other.weights()[i] && weights()[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && sumSquareDelta > similarityCutoff) return null;
         return new SimilarityResult(id(), other.id(), count(), other.count(), intersection, sumSquareDelta);
     }
@@ -36,11 +32,7 @@ class SparseWeightedInput implements WeightedInput {
         int len = Math.min(weights().length, other.weights().length);
         double sumSquareDelta = Intersections.sumSquareDelta(weights(), other.weights(), len);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights()[i] == other.weights()[i] && weights()[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && sumSquareDelta > similarityCutoff) return null;
         return new SimilarityResult(id(), other.id(), count(), other.count(), intersection, sumSquareDelta);
     }
@@ -54,11 +46,7 @@ class SparseWeightedInput implements WeightedInput {
         int len = Math.min(thisWeights.length, otherWeights.length);
         double cosineSquares = Intersections.cosineSquareSkip(thisWeights, otherWeights, len, skipValue);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights()[i] == other.weights()[i] && weights()[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && (cosineSquares == 0 || cosineSquares < similarityCutoff)) return null;
         return new SimilarityResult(id(), other.id(), count(), other.count(), intersection, cosineSquares);
     }
@@ -72,11 +60,7 @@ class SparseWeightedInput implements WeightedInput {
         int len = Math.min(thisWeights.length, otherWeights.length);
         double cosineSquares = Intersections.cosineSquare(thisWeights, otherWeights, len);
         long intersection = 0;
-        /* todo
-        for (int i = 0; i < len; i++) {
-            if (weights()[i] == other.weights()[i] && weights()[i] != 0d) intersection++;
-        }
-        */
+
         if (similarityCutoff >= 0d && (cosineSquares == 0 || cosineSquares < similarityCutoff)) return null;
         return new SimilarityResult(id(), other.id(), count(), other.count(), intersection, cosineSquares);
     }
