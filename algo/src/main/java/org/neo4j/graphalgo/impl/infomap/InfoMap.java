9cd742aa4bfdc2ede6a9832c8167a9be1c574da3
Martin Knobloch <mknblch@gmail.com>
2018-12-13 18:41:31 +0100

    performance tests

--- algo/src/main/java/org/neo4j/graphalgo/impl/infomap/InfoMap.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/infomap/InfoMap.java
@@ -1,10 +1,8 @@
 package org.neo4j.graphalgo.impl.infomap;
 
-import com.carrotsearch.hppc.IntObjectMap;
-import com.carrotsearch.hppc.IntObjectScatterMap;
-import com.carrotsearch.hppc.IntScatterSet;
-import com.carrotsearch.hppc.IntSet;
+import com.carrotsearch.hppc.*;
 import com.carrotsearch.hppc.cursors.IntObjectCursor;
+import com.carrotsearch.hppc.procedures.IntDoubleProcedure;
 import com.carrotsearch.hppc.procedures.IntProcedure;
 import org.neo4j.graphalgo.api.Graph;
 import org.neo4j.graphalgo.api.NodeWeights;
@@ -136,7 +134,6 @@ public class InfoMap extends Algorithm<InfoMap> {
             return true;
         });
         this.sQi = d[0];
-
     }
 
     /**
@@ -273,7 +270,7 @@ public class InfoMap extends Algorithm<InfoMap> {
     private double delta(Module j, Module k) {
         double ni = j.nodes.size() + k.nodes.size();
         double pi = j.p + k.p;
-        double wi = j.w + k.w - interModW(j, k);
+        double wi = j.w + k.w - j.delta(k); //interModW(j, k);
         double qi = tau * pi * (nodeCount - ni) / n1 + tau1 * wi;
         return plogp(qi - j.q - k.q + sQi)
                 - plogp(sQi)
@@ -321,6 +318,8 @@ public class InfoMap extends Algorithm<InfoMap> {
         // exit probability with teleport
         double q;
 
+        IntDoubleMap wil = new IntDoubleScatterMap();
+
         public Module(int startNode) {
             this.index = startNode;
             nodes = new IntScatterSet();
@@ -329,7 +328,9 @@ public class InfoMap extends Algorithm<InfoMap> {
             // sum of all weights
             graph.forEachRelationship(startNode, D, (s, t, r) -> {
                 if (s != t) {
-                    sumW.v += weights.weightOf(s, t);
+                    final double v = weights.weightOf(s, t);
+                    sumW.v += v;
+                    wil.put(t, v * pageRank.weightOf(s) + weights.weightOf(t, s) * pageRank.weightOf(t));
                 }
                 return true;
             });
@@ -338,10 +339,32 @@ public class InfoMap extends Algorithm<InfoMap> {
             q = tau * p + tau1 * w;
         }
 
+        public double delta(Module other) {
+            final Pointer.DoublePointer wi = Pointer.wrap(0.);
+            wil.forEach((IntDoubleProcedure) (key, value) -> {
+                if (communities[key] == other.index) {
+                    wi.v += value ;
+                }
+            });
+            return wi.v;
+        }
+
         public void merge(Module other) {
             final int ni = nodes.size() + other.nodes.size();
             p += other.p;
-            w += other.w - interModW(this, other);
+            wil.putAll(other.wil);
+//
+//            final Pointer.DoublePointer wi = Pointer.wrap(0.);
+//            wil.forEach((IntDoubleProcedure) (key, value) -> {
+//                if (communities[key] == other.index) {
+//                    wi.v += value ;
+//                }
+//            });
+//            System.out.println("wi = " + wi.v);
+//            System.out.println("interModW(this, other) = " + interModW(this, other));
+
+//            w += other.w - wil;
+            w += other.w - delta(other); //interModW(this, other);
             sQi -= q + other.q;
             q = tau * p * (nodeCount - ni) / n1 + tau1 * w;
             sQi += q;
