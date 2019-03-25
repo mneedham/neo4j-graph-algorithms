8f755c6ddf1f59b93b89b7db550317daec4eaef4
Mark Needham <m.h.needham@gmail.com>
2019-03-25 12:31:33 +0000

    algo.asPath

---
+++ algo/src/main/java/org/neo4j/graphalgo/UtilityProc.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
+ * <p>
+ * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
+ * <p>
+ * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo;
+
+import org.neo4j.graphalgo.impl.walking.WalkPath;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Path;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.Collections.singletonMap;
+
+public class UtilityProc {
+
+    @Context
+    public GraphDatabaseService db;
+
+    @Procedure("algo.asPath")
+    @Description("CALL algo.asPath - returns a path for the provided node ids and weights")
+    public Stream<Path> list(
+            @Name(value = "nodeIds", defaultValue = "") List<Long> nodeIds,
+            @Name(value = "weights", defaultValue = "[]") List<Double> weights) {
+
+        if (weights.size() > 0) {
+            return Stream.of(WalkPath.toPath((GraphDatabaseAPI) db,
+                    nodeIds.stream().mapToLong(l -> l).toArray(),
+                    weights.stream().mapToDouble(d -> d).toArray()));
+        } else {
+            return Stream.of(WalkPath.toPath((GraphDatabaseAPI) db,
+                    nodeIds.stream().mapToLong(l -> l).toArray()));
+        }
+
+
+    }
+
+
+}
